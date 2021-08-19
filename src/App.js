cf5b78f293d75a1fe033516d8b9c700883713a6b
Christopher Jones <tkcwebdev@gmail.com>
2021-08-19 14:45:28 -0700

    master: Initial commit and deleting master branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h2>Ultimate CSS Grid Course</h2>
+			<p>This is the template you can use to follow along the course.</p>
+			<div className='grid'>
+				<div>Item 1</div>
+				<div>Item 2</div>
+				<div>Item 3</div>
+				<div>Item 4</div>
+				<div>Item 5</div>
+				<div>Item 6</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
