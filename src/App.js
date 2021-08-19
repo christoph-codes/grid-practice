2463d4aab2c5a868b94ed2d1b048ebc0e7d1c938
Christopher Jones <tkcwebdev@gmail.com>
2021-08-19 14:52:03 -0700

    main: Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.scss';
+
+function App() {
+	return (
+		<div className='App'>
+			<h2>Ultimate CSS Grid Course</h2>
+			<p>This is the template you can use to follow along the course.</p>
+			<div className='grid'>
+				<div>Item 1</div>
+				<div>Item 2</div>
+				<div>Item 3</div>
+				<div>Item 4</div>
+				<div>Item 5</div>
+				<div>Item 6</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
