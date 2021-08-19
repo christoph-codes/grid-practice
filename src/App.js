e59d730c54a5a3524997d3eda1adb997b14fdda9
Christopher Jones <tkcwebdev@gmail.com>
2021-08-19 16:08:37 -0700

    main: Finished exercise 1 & 2

--- src/App.js
+++ src/App.js
@@ -2,18 +2,44 @@ import './App.scss';
 
 function App() {
 	return (
-		<div className='App'>
-			<h2>Ultimate CSS Grid Course</h2>
-			<p>This is the template you can use to follow along the course.</p>
-			<div className='grid'>
-				<div>Item 1</div>
-				<div>Item 2</div>
-				<div>Item 3</div>
-				<div>Item 4</div>
-				<div>Item 5</div>
-				<div>Item 6</div>
-			</div>
-		</div>
+		<main className='App'>
+			<section className='working'>
+				<h2>Ultimate CSS Grid Course</h2>
+				<p>
+					This is the template you can use to follow along the course.
+				</p>
+				<article className='grid'>
+					<div>Item 1</div>
+					<div>Item 2</div>
+					<div>Item 3</div>
+					<div>Item 4</div>
+					<div>Item 5</div>
+					<div>Item 6</div>
+				</article>
+			</section>
+			<section className='exercise1'>
+				<h2>Excercise 1</h2>
+				<article className='grid'>
+					<div>Item 1</div>
+					<div>Item 2</div>
+					<div>Item 3</div>
+					<div>Item 4</div>
+				</article>
+			</section>
+			<section className='exercise2'>
+				<h2>Excercise 2</h2>
+				<article className='grid'>
+					<div>Item 1</div>
+					<div>Item 2</div>
+					<div>Item 3</div>
+					<div>Item 4</div>
+					<div>Item 5</div>
+					<div>Item 6</div>
+					<div>Item 7</div>
+					<div>Item 8</div>
+				</article>
+			</section>
+		</main>
 	);
 }
 
